3a9752a3837f5e6a2465238705d006e229f5ab9f
Jeremy Klein <jklein24@gmail.com>
2025-05-16 14:48:34 -0700

    Bump python lib version.

--- generated/python/umaaas_api/__init__.py
+++ generated/python/umaaas_api/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 # import apis into sdk package
 from umaaas_api.api.api_tokens_api import APITokensApi
--- generated/python/umaaas_api/configuration.py
+++ generated/python/umaaas_api/configuration.py
@@ -556,7 +556,7 @@ conf = umaaas_api.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: 0.0.15".\
+               "SDK Package Version: 0.0.16".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
